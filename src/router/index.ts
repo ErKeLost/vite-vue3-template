d66333ab07ca41671080ff931211f31de8640b06
erkelost <1256029807@qq.com>
2022-04-17 08:28:23 +0800

    chore(router): 🫠🫠🫠update router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,60 +1,61 @@
-// import { App } from 'vue'
-// import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
-// import { RedirectRoute } from '@/router/base'
+import { App } from 'vue'
+import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
+import { RedirectRoute } from '@/router/base'
 // import { PageEnum } from '@/enums/pageEnum'
-// import { createRouterGuards } from './router-guards'
+import { createRouterGuards } from './router-guards'
 
-// const modules = import.meta.globEager('./modules/**/*.ts')
+const modules = import.meta.globEager('./modules/**/*.ts')
 
-// const routeModuleList: RouteRecordRaw[] = []
+const routeModuleList: RouteRecordRaw[] = []
 
-// Object.keys(modules).forEach((key) => {
-//   const mod = modules[key].default || {}
-//   const modList = Array.isArray(mod) ? [...mod] : [mod]
-//   routeModuleList.push(...modList)
-// })
+Object.keys(modules).forEach((key) => {
+  const mod = modules[key].default || {}
+  const modList = Array.isArray(mod) ? [...mod] : [mod]
+  routeModuleList.push(...modList)
+})
 
-// function sortRoute(a, b) {
-//   return (a.meta?.sort || 0) - (b.meta?.sort || 0)
-// }
+function sortRoute(a, b) {
+  return (a.meta?.sort || 0) - (b.meta?.sort || 0)
+}
 
-// routeModuleList.sort(sortRoute)
+routeModuleList.sort(sortRoute)
 
-// export const RootRoute: RouteRecordRaw = {
-//   path: '/',
-//   name: 'Root',
-//   redirect: PageEnum.BASE_HOME,
-//   meta: {
-//     title: 'Root'
-//   }
-// }
+export const RootRoute: RouteRecordRaw = {
+  path: '/',
+  name: 'Root',
+  // redirect: PageEnum.BASE_HOME,
+  redirect: '',
+  meta: {
+    title: 'Root'
+  }
+}
 
-// export const LoginRoute: RouteRecordRaw = {
-//   path: '/login',
-//   name: 'Login',
-//   component: () => import('@/views/login/index.vue'),
-//   meta: {
-//     title: '登录'
-//   }
-// }
+export const LoginRoute: RouteRecordRaw = {
+  path: '/login',
+  name: 'Login',
+  component: () => import('@/views/login/index.vue'),
+  meta: {
+    title: '登录'
+  }
+}
 
-// //需要验证权限
-// export const asyncRoutes = [...routeModuleList]
+//需要验证权限
+export const asyncRoutes = [...routeModuleList]
 
-// //普通路由 无需验证权限
-// export const constantRouter: any[] = [LoginRoute, RootRoute, RedirectRoute]
+//普通路由 无需验证权限
+export const constantRouter: any[] = [LoginRoute, RootRoute, RedirectRoute]
 
-// const router = createRouter({
-//   history: createWebHashHistory(''),
-//   routes: constantRouter,
-//   strict: true,
-//   scrollBehavior: () => ({ left: 0, top: 0 })
-// })
+const router = createRouter({
+  history: createWebHashHistory(''),
+  routes: constantRouter,
+  strict: true,
+  scrollBehavior: () => ({ left: 0, top: 0 })
+})
 
-// export function setupRouter(app: App) {
-//   app.use(router)
-//   // 创建路由守卫
-//   createRouterGuards(router)
-// }
+export function setupRouter(app: App) {
+  app.use(router)
+  // 创建路由守卫
+  createRouterGuards(router)
+}
 
-// export default router
+export default router
