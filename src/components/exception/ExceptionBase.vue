a74fdb9b6b8a3b66912eea1907c4b07bab18074d
erkelost <1256029807@qq.com>
2022-04-17 14:17:00 +0800

    ci(exception): 🐰🐰🐰test svg custrom

--- src/components/exception/ExceptionBase.vue
+++ src/components/exception/ExceptionBase.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="flex-col-center wh-full">
     <div class="text-400px text-primary">
-      <icon-custom-no-permission v-if="type === '403'" />
+      <!-- <icon-custom-no-permission v-if="type === '403'" />
       <icon-custom-not-found v-if="type === '404'" />
-      <icon-custom-service-error v-if="type === '500'" />
+      <icon-custom-service-error v-if="type === '500'" /> -->
+      <icon-custom-no-permission />
+      <icon-custom-not-found />
+      <icon-custom-service-error />
     </div>
     <!-- <router-link :to="{ name: routeHomePath }">
       <n-button type="primary">回到首页</n-button>
