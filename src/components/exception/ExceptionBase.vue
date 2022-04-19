68221dceccdb5217b0ac0c9f97f2d5816e29a849
erkelost <1256029807@qq.com>
2022-04-19 12:59:52 +0800

    refactor(img): rebuild assets

--- src/components/exception/ExceptionBase.vue
+++ src/components/exception/ExceptionBase.vue
@@ -1,12 +1,9 @@
 <template>
   <div class="flex-col-center wh-full">
     <div class="text-400px text-primary">
-      <!-- <icon-custom-no-permission v-if="type === '403'" />
+      <icon-custom-not-permission v-if="type === '403'" />
       <icon-custom-not-found v-if="type === '404'" />
-      <icon-custom-service-error v-if="type === '500'" /> -->
-      <icon-custom-no-permission />
-      <icon-custom-not-found />
-      <icon-custom-service-error />
+      <icon-custom-not-service v-if="type === '500'" />
     </div>
     <!-- <router-link :to="{ name: routeHomePath }">
       <n-button type="primary">回到首页</n-button>
--- types/components.d.ts
+++ types/components.d.ts
@@ -11,6 +11,8 @@ declare module 'vue' {
     ExceptionBase: typeof import('./../src/components/exception/ExceptionBase.vue')['default']
     IconCustomNoPermission: typeof import('~icons/custom/no-permission')['default']
     IconCustomNotFound: typeof import('~icons/custom/not-found')['default']
+    IconCustomNotPermission: typeof import('~icons/custom/not-permission')['default']
+    IconCustomNotService: typeof import('~icons/custom/not-service')['default']
     IconCustomServiceError: typeof import('~icons/custom/service-error')['default']
     ParentLayout: typeof import('./../src/layout/parentLayout.vue')['default']
     Undefined: typeof import('./../src/layout/index.vue')['default']
