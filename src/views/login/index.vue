f923cf884f6b9f92eff7ebcd84168e490400af70
erkelost <1256029807@qq.com>
2022-04-23 07:32:22 +0800

    chore(unocss): update unocss

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,10 +1,14 @@
 <template>
-  <div class="container">我是login页面</div>
+  <div h-full bg-pink-300>
+    <div class="text-8xl">
+      <div i-carbon-campsite inline-block />
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts"></script>
-<style lang="scss">
-.container {
-  color: $theme-color;
+<style lang="scss" scoped>
+.container-login {
+  background-color: $theme-color;
 }
 </style>
