d2a92c1e0b4263bac210ff0e38d4710f46c9726b
erkelost <1256029807@qq.com>
2022-04-23 19:38:48 +0800

    chore(login): base layout 💕 💕 💕

--- src/views/login/cpns/login-account.vue
+++ src/views/login/cpns/login-account.vue
@@ -2,21 +2,13 @@
   <div h-full text-center flex select-none all:transition-400>
     <div ma>
       <div text-5xl fw100 animate-bounce-alt animate-count-infinite animate-1s>
-        <icon-custom-sign />
-      </div>
-      <div op30 text-lg fw300 m1>The instant on-demand Atomic CSS engine.</div>
-      <div m2 flex justify-center text-2xl op30 hover="op80">
-        <a
-          i-carbon-logo-github
-          text-inherit
-          href="https://github.com/unocss/unocss"
-          target="_blank"
-        ></a>
+        <img
+          w-100
+          src="https://jzzx-docs.netlify.app/assets/adny.e203de98.png"
+          alt=""
+        />
       </div>
     </div>
-  </div>
-  <div absolute bottom-5 right-0 left-0 text-center op30 fw300>
-    ERKELOST & ADNY @COPYRIGHT 2022
   </div>
 </template>
 
--- src/views/login/cpns/login-preview.vue
+++ src/views/login/cpns/login-preview.vue
@@ -1,22 +1,28 @@
 <template>
-  <div h-full text-center flex select-none all:transition-400>
+  <div
+    h-full
+    text-center
+    bg-blue-200
+    flex
+    flex-col
+    select-none
+    all:transition-400
+  >
     <div ma>
       <div text-5xl fw100 animate-bounce-alt animate-count-infinite animate-1s>
         <icon-custom-sign />
       </div>
-      <div op30 text-lg fw300 m1>The instant on-demand Atomic CSS engine.</div>
+      <div text-lg fw300 m1>ADNY 前中后台解决方案</div>
       <div m2 flex justify-center text-2xl op30 hover="op80">
         <a
           i-carbon-logo-github
           text-inherit
-          href="https://github.com/unocss/unocss"
+          href="https://github.com/ErKeLost?tab=repositories"
           target="_blank"
         ></a>
       </div>
     </div>
-  </div>
-  <div absolute bottom-5 right-0 left-0 text-center op30 fw300>
-    ERKELOST & ADNY @COPYRIGHT 2022
+    <div text-center fw300 m-5>ERKELOST & ADNY @COPYRIGHT 2022</div>
   </div>
 </template>
 
