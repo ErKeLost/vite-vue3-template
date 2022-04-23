819c220c78230597d7ed8d25dbb90f9dec064b62
erkelost <1256029807@qq.com>
2022-04-23 08:44:16 +0800

    chore(exception): reps exceptoon paages😊 😊 😊

--- src/components/exception/ExceptionBase.vue
+++ src/components/exception/ExceptionBase.vue
@@ -1,10 +1,13 @@
 <template>
-  <div class="flex-col-center wh-full">
-    <div class="text-400px text-primary">
-      <icon-custom-not-permission v-if="type === '403'" />
-      <icon-custom-not-found v-if="type === '404'" />
-      <icon-custom-not-service v-if="type === '500'" />
+  <div h-full text-center flex select-none>
+    <div ma>
+      <div text-400px text-primary>
+        <icon-custom-not-permission v-if="type === '403'" />
+        <icon-custom-not-found v-if="type === '404'" />
+        <icon-custom-not-service v-if="type === '500'" />
+      </div>
     </div>
+
     <!-- <router-link :to="{ name: routeHomePath }">
       <n-button type="primary">回到首页</n-button>
     </router-link> -->
